164a87851953bba5598de2984bf713d2abe5d830
Sergey Tulentsev <sergei.tulentsev@gmail.com>
2022-05-27 11:52:05 +0100

    add new craftable resources

--- greasemonkey/resource_auto_converters.js
+++ greasemonkey/resource_auto_converters.js
@@ -16,6 +16,7 @@ function restoreSettings() {
             "gm-rac-convertMinerals",
             "gm-rac-convertIron",
             "gm-rac-makeSteel",
+            "gm-rac-makeConcrete",
             "gm-rac-makeGear",
             "gm-rac-makeAlloy",
             "gm-rac-makeScaffold",
@@ -23,6 +24,7 @@ function restoreSettings() {
             "gm-rac-makeParchments",
             "gm-rac-makeManuscripts",
             "gm-rac-makeCompendiums",
+            "gm-rac-makeBlueprints",
             "gm-rac-sendHunters",
             "gm-rac-autoBuyBuildings"
 
@@ -48,6 +50,7 @@ function saveSettings() {
         "gm-rac-convertMinerals",
         "gm-rac-convertIron",
         "gm-rac-makeSteel",
+        "gm-rac-makeConcrete",
         "gm-rac-makeGear",
         "gm-rac-makeAlloy",
         "gm-rac-makeScaffold",
@@ -55,6 +58,7 @@ function saveSettings() {
         "gm-rac-makeParchments",
         "gm-rac-makeManuscripts",
         "gm-rac-makeCompendiums",
+        "gm-rac-makeBlueprints",
         "gm-rac-sendHunters",
         "gm-rac-autoBuyBuildings"
 
@@ -129,13 +133,15 @@ function startConverters() {
         slab: {triggerId: ".0.5.1.0.4.5.0.1", buttonId: ".0.5.1.0.4.6.0", toggleId: "gm-rac-convertMinerals", sourceSelector: "resource_minerals"},
         plate: {triggerId: ".0.5.1.0.5.5.0.1", buttonId: ".0.5.1.0.5.6.0", toggleId: "gm-rac-convertIron", sourceSelector: "resource_iron"},
         steel: {triggerId: ".0.5.1.0.6.3.0.1", buttonId: ".0.5.1.0.6.6.0", toggleId: "gm-rac-makeSteel", sourceSelector: "resource_coal"},
+        concrete: {triggerId: ".0.5.1.0.7.3.0.1", buttonId: ".0.5.1.0.7.6.0", toggleId: "gm-rac-makeConcrete"},
         gear: {triggerId: ".0.5.1.0.8.5.0.1", buttonId: ".0.5.1.0.8.6.0", toggleId: "gm-rac-makeGear"},
         alloy: {triggerId: ".0.5.1.0.9.5.0.1", buttonId: ".0.5.1.0.9.6.0", toggleId: "gm-rac-makeAlloy", sourceSelector: "resource_titanium"},
         scaffold: {triggerId: ".0.5.1.0.b.5.0.1", buttonId: ".0.5.1.0.b.6.0", toggleId: "gm-rac-makeScaffold"},
         ship: {triggerId: ".0.5.1.0.c.3.0.1", buttonId: ".0.5.1.0.c.6.0", toggleId: "gm-rac-makeShip"},
-        parchment: {triggerId: ".0.5.1.0.f.3.0.1", buttonId: ".0.5.1.0.f.6.0", toggleId: "gm-rac-makeParchments"},
-        manuscripts: {triggerId: ".0.5.1.0.g.5.0.1", buttonId: ".0.5.1.0.g.6.0", toggleId: "gm-rac-makeManuscripts"},
+        parchment: {triggerId: ".0.5.1.0.f.5.0.1", buttonId: ".0.5.1.0.f.6.0", toggleId: "gm-rac-makeParchments"},
+        manuscripts: {triggerId: ".0.5.1.0.g.4.0.1", buttonId: ".0.5.1.0.g.6.0", toggleId: "gm-rac-makeManuscripts"},
         compendiums: {triggerId: ".0.5.1.0.h.3.0.1", buttonId: ".0.5.1.0.h.6.0", toggleId: "gm-rac-makeCompendiums"},
+        blueprint: {triggerId: ".0.5.1.0.i.3.0.1", buttonId: ".0.5.1.0.i.6.0", toggleId: "gm-rac-makeBlueprints"},
         hunters: {
             triggerId: ".0.2.0.1.0",
             buttonId: ".0.2.0.1.0",
@@ -181,21 +187,22 @@ function createUI() {
         <ul style="list-style-type: none">
             <li>
                 <input type="checkbox" id="gm-rac-convertCatnip" /><label for="gm-rac-convertCatnip">Make wood</label>
-                <input type="checkbox" id="gm-rac-convertCatnipAtCap" /><label for="gm-rac-convertCatnipAtCap">At cap</label>
+<!--                <input type="checkbox" id="gm-rac-convertCatnipAtCap" /><label for="gm-rac-convertCatnipAtCap">At cap</label>-->
             </li>
             <li>
                 <input type="checkbox" id="gm-rac-convertWood" /><label for="gm-rac-convertWood">Make beam</label>
-                <input type="checkbox" id="gm-rac-convertWoodAtCap" /><label for="gm-rac-convertWoodAtCap">At cap</label>
+<!--                <input type="checkbox" id="gm-rac-convertWoodAtCap" /><label for="gm-rac-convertWoodAtCap">At cap</label>-->
             </li>
             <li>
                 <input type="checkbox" id="gm-rac-convertMinerals" /><label for="gm-rac-convertMinerals">Make slab</label>
-                <input type="checkbox" id="gm-rac-convertMineralsAtCap" /><label for="gm-rac-convertMineralsAtCap">At cap</label>
+<!--                <input type="checkbox" id="gm-rac-convertMineralsAtCap" /><label for="gm-rac-convertMineralsAtCap">At cap</label>-->
             </li>
             <li>
                 <input type="checkbox" id="gm-rac-convertIron" /><label for="gm-rac-convertIron">Make plate</label>
-                <input type="checkbox" id="gm-rac-convertIronAtCap" /><label for="gm-rac-convertIronAtCap">At cap</label>
+<!--                <input type="checkbox" id="gm-rac-convertIronAtCap" /><label for="gm-rac-convertIronAtCap">At cap</label>-->
             </li>
             <li><input type="checkbox" id="gm-rac-makeSteel" /><label for="gm-rac-makeSteel">Make steel</label></li>
+            <li><input type="checkbox" id="gm-rac-makeConcrete" /><label for="gm-rac-makeConcrete">Make concrete</label></li>
             <li><input type="checkbox" id="gm-rac-makeGear" /><label for="gm-rac-makeGear">Make gear</label></li>
             <li><input type="checkbox" id="gm-rac-makeAlloy" /><label for="gm-rac-makeAlloy">Make alloy</label></li>
             <li><input type="checkbox" id="gm-rac-makeScaffold" /><label for="gm-rac-makeScaffold">Make scaffold</label></li>
@@ -203,6 +210,7 @@ function createUI() {
             <li><input type="checkbox" id="gm-rac-makeParchments" /><label for="gm-rac-makeParchments">Make parchments</label></li>
             <li><input type="checkbox" id="gm-rac-makeManuscripts" /><label for="gm-rac-makeManuscripts">Make manuscripts</label></li>
             <li><input type="checkbox" id="gm-rac-makeCompendiums" /><label for="gm-rac-makeCompendiums">Make compendiums</label></li>
+            <li><input type="checkbox" id="gm-rac-makeBlueprints" /><label for="gm-rac-makeBlueprints">Make blueprints</label></li>
             <li><input type="checkbox" id="gm-rac-sendHunters" /><label for="gm-rac-sendHunters">Send hunters when have </label><input type="number" value="27" id="gm-rac-needHunters" style="width:50px" /></li>
             <li><input type="checkbox" id="gm-rac-autoBuyBuildings" /><label for="gm-rac-autoBuyBuildings">Auto-buy buildings</label></li>
             <li>
